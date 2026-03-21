76672356b0b6cd65136a2230c26aa9af58044632
ojitos369 <ojitos369@gmail.com>
2026-03-20 23:39:06 -0600

    🔧 Revert vite config base + use migrate_view.py to build for production

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/media/dist/',
   plugins: [react()],
   server: {
     allowedHosts: ['reapi.ojitos369.com']
